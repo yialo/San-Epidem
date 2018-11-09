6dd7ff6ad5b558c80f65bc47d931dddc96fadc82
Aleksei Arro <alexey.arro@waytostart.ru>
2018-11-09 16:14:36 +0300

    JS hooks added

--- source/js/page-header-menu.js
+++ source/js/page-header-menu.js
@@ -4,27 +4,27 @@ var buttonClose = document.querySelector('.page-header__menu-button--close');
 
 buttonOpen.addEventListener('click', function(evt) {
     evt.preventDefault();
-    buttonOpen.classList.toggle('page-header__menu-button--js-hidden');
-    buttonClose.classList.toggle('page-header__menu-button--js-hidden');
-    menu.classList.toggle('navigation--js-closed');
+    buttonOpen.classList.toggle('is-hidden');
+    buttonClose.classList.toggle('is-hidden');
+    menu.classList.toggle('is-hidden');
     buttonClose.focus();
   });
 
 buttonClose.addEventListener('click', function(evt) {
     evt.preventDefault();
-    buttonOpen.classList.toggle('page-header__menu-button--js-hidden');
-    buttonClose.classList.toggle('page-header__menu-button--js-hidden');
-    menu.classList.toggle('navigation--js-closed');
+    buttonOpen.classList.toggle('is-hidden');
+    buttonClose.classList.toggle('is-hidden');
+    menu.classList.toggle('is-hidden');
     buttonOpen.focus();
 });
 
 
 window.addEventListener('keydown', function(evt) {
-    if ((evt.keyCode === 27) && (!menu.classList.contains('navigation--js-closed'))) {
+    if ((evt.keyCode === 27) && (!menu.classList.contains('is-hidden'))) {
         evt.preventDefault();
-        menu.classList.toggle('navigation--js-closed');
-        buttonOpen.classList.toggle('page-header__menu-button--js-hidden');
-        buttonClose.classList.toggle('page-header__menu-button--js-hidden');
+        menu.classList.toggle('is-hidden');
+        buttonOpen.classList.toggle('is-hidden');
+        buttonClose.classList.toggle('is-hidden');
         buttonOpen.focus();
     }
 });
