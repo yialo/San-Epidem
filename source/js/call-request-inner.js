16d8d5c9bf245611b7c449fd0a121aa39746b212
Aleksei Arro <alexey.arro@waytostart.ru>
2018-11-23 16:23:12 +0300

    Modal close mechanics fixed on inner pages

--- source/js/call-request-inner.js
+++ source/js/call-request-inner.js
@@ -1,6 +1,5 @@
 var headerModalCallButton = document.querySelector('.page-header__phone-button');
 var footerModalCallButton = document.querySelector('.page-footer__button');
-var problemsButton = document.querySelector('.problems__button')
 var contactsButton = document.querySelector('.contacts__button');
 var overlay = document.querySelector('.overlay');
 var modalCallRequest = document.querySelector('.call-request');
@@ -11,12 +10,6 @@ var callRequestPhone = document.querySelector('.call-request__field--phone');
 var callRequestCheckbox = document.querySelector('.call-request__checkbox');
 
 headerModalCallButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    overlay.classList.add('is-visible');
-    modalCallRequest.classList.add('is-visible');
-});
-
-problemsButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     overlay.classList.add('is-visible');
     modalCallRequest.classList.add('is-visible');
